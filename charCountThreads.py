9418ca50f737f7f0b3c18d1dc70e48eae7b13370
Muhammad usman <mu896626@gmail.com>
2025-02-05 15:02:48 +0500

    updated code with multiprocessing

--- charCountThreads.py
+++ charCountThreads.py
@@ -1,56 +1,60 @@
 import sys
 import time
-from threading import Thread
+from multiprocessing import Process, Manager
 
-vovels = 0
-chars = 0
-spaces = 0
-punchs= 0
-def processFile(data):
-    # intial variables
-    global vovels, chars, spaces, punchs
+def processFile(data, results):
+    vovels = chars = spaces = punchs = 0
 
     for char in data:
         currentChar = ord(char)
-        if (currentChar >= 97 and currentChar<=122 ) or (currentChar >= 65 and currentChar <=90):
-            chars +=1
-            if(currentChar == 97 or currentChar == 101 or currentChar == 105 or currentChar == 111 or currentChar == 117 ):
-                vovels +=1
-        elif(currentChar == 32):
-            spaces +=1
-        elif(currentChar == 33 or currentChar == 34 or currentChar ==39 or currentChar ==40 or currentChar == 41 or currentChar == 42 or currentChar == 44 or currentChar == 45 or currentChar == 46 or currentChar == 49 or currentChar == 58 or currentChar == 59 or currentChar == 60 or currentChar == 62 or currentChar == 63 or currentChar == 91 or currentChar == 93 or currentChar == 95):
+        if (currentChar >= 97 and currentChar <= 122) or (currentChar >= 65 and currentChar <= 90):
+            chars += 1
+            if currentChar in (97, 101, 105, 111, 117):  # a, e, i, o, u
+                vovels += 1
+        elif currentChar == 32:
+            spaces += 1
+        elif currentChar in (33, 34, 39, 40, 41, 42, 44, 45, 46, 49, 58, 59, 60, 62, 63, 91, 93, 95):
             punchs += 1
-        
-    
-    
 
-# read file
-with open(sys.argv[1], 'r') as file:
-    data = file.read()
+    results.append((chars, spaces, punchs, vovels))
 
-# creating Partitions
-data1 = data[0:len(data)//2]
-data2 = data[len(data)//2+1:len(data)]
+if __name__ == "__main__": 
+    # Read file
+    with open(sys.argv[1], 'r') as file:
+        data = file.read()
 
-start = time.time()
+    # Create partitions
+    data1 = data[:len(data)//2]
+    data2 = data[len(data)//2:]
 
-# creating threads
-t1 = Thread(target=processFile, args=(data1,))
-t2 = Thread(target=processFile, args=(data2,))
-t1.start()
-t2.start()
+    start = time.time()
 
-# get the returning values from threads
-part1 = t1.join()
-part2 = t2.join()
+    # Using multiprocessing
+    with Manager() as manager:
+        results = manager.list()
 
-print("total: ",chars)
-print("Spaces: ",spaces)
-print("Punctuation: ",punchs)
-print("total length: ", len(data))
-print("Total Characters: ",len(data) - spaces)
-print('Word Count: ', spaces+1)
+        p1 = Process(target=processFile, args=(data1, results))
+        p2 = Process(target=processFile, args=(data2, results))
 
-end = time.time()
+        p1.start()
+        p2.start()
 
-print ("\n\nTime taken for threads: ", end-start)
+        p1.join()
+        p2.join()
+
+        # Aggregate results
+        total_chars = sum(r[0] for r in results)
+        total_spaces = sum(r[1] for r in results)
+        total_punchs = sum(r[2] for r in results)
+        total_vovels = sum(r[3] for r in results)
+
+        print("Total Characters:", total_chars)
+        print("Spaces:", total_spaces)
+        print("Punctuation:", total_punchs)
+        print("Total length:", len(data))
+        print("Characters (excluding spaces):", len(data) - total_spaces)
+        print("Word Count:", total_spaces + 1)
+        print("Vowels:", total_vovels)
+
+    end = time.time()
+    print("\nTime taken with multiprocessing:", end - start)
